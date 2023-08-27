d865fc9bf8e5cf7758387765b88ef9bd996968d7
JYROH <35508595+ghgt1@users.noreply.github.com>
2023-08-28 02:51:00 +0900

    [level 3] Title: 네트워크, Time: 8.49 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/README.md
+++ 프로그래머스/lv3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 1.58 ms
+메모리: 10.3 MB, 시간: 8.49 ms
 
 ### 구분
 
--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,29 +1,24 @@
+import copy
 def solution(n, computers):
-    answer = 0
-    # 인접행렬 to 인접 리스트
-    graph = [[] for _ in range(n)]
-    for k in range(n):
-        for u in range(n):
-            if computers[k][u] == 1:
-                if u not in graph[k]:
-                    graph[k].append(u)
-                if k not in graph[u]:
-                    graph[u].append(k)
-    print(graph)
-
-        
-    def DFS(v,visited):
-        visited[v] = True
-        for u in graph[v]:
-            if visited[u] == False:
-                DFS(u,visited)
-
+    graph = [[] for _ in range(n+1)]
     res = []
     for k in range(n):
-        visited = [False] * n
-        DFS(k,visited)
-        if visited in res:
-            continue
-        res.append(visited)
-        
+        for u in range(n):
+            if computers[k][u] == 1 and k!=u:
+                graph[k].append(u)
+    print(graph)
+    
+    def dfs(graph,v,visited):
+        visited[v] = True
+        for k in graph[v]:
+            if not visited[k]:
+                dfs(graph,k,visited)
+    
+    for k in range(n):
+        graph2 = copy.deepcopy(graph)
+        visited = [False]*n
+        dfs(graph2,k,visited)
+        if visited not in res:
+            res.append(visited)
+    
     return len(res)
