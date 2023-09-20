71b9d274091604d73cd89d60b4d396db65c046c1
JYROH <35508595+ghgt1@users.noreply.github.com>
2023-09-20 22:13:52 +0900

    [level 2] Title: 타겟 넘버, Time: 11.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,15 +1,20 @@
-
 def solution(numbers, target):
+    global answer
     answer = 0
-#     가지치기 해도 되고
-#     아니면 그냥 모든 경우의 수 구해도 됨
-# 같은말이지;
-# 이건 한 가지 쭉 내려가야하니. DFS
-    tmp = [0]
-    for k in numbers:
-        tmp2 = []
-        for q in tmp:
-            tmp2.append(q+k)
-            tmp2.append(q-k)
-        tmp = tmp2
-    return tmp.count(target)
+    l1 = []
+    def DFS():
+        global answer
+        if len(numbers) == len(l1):
+            if sum(l1) == target:
+                answer+=1
+                return
+            else:
+                return
+        l1.append(numbers[len(l1)])
+        DFS()
+        l1.pop()
+        l1.append(-numbers[len(l1)])
+        DFS()
+        l1.pop()
+    DFS()
+    return answer
